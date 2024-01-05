2767621cb9216f61fe40325b199b04044acf94b6
axlkun <axelcruz.dev@gmail.com>
2024-01-04 23:14:40 -0600

    add general meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,34 @@ import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'es',
+      },
+      meta: [
+        { 'http-equiv': 'content-language', content: 'es' },
+        // ...otras etiquetas meta
+      ],
+      title: 'Axel Cruz | Fullstack Developer',
+
+      script: [
+        {
+          async: true,
+          src: "https://www.googletagmanager.com/gtag/js?id=G-WCSKBPW0D4"
+        },
+        {
+          innerHTML: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-WCSKBPW0D4');
+          `
+        }]
+    }
+  },
+
   build: {
     transpile: ['vuetify'],
   },
