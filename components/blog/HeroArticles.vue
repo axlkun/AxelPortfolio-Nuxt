d8687d4f4764a55f37c3d5beb3106130d08ab68c
axlkun <axelcruz.dev@gmail.com>
2024-08-08 18:25:25 -0600

    fix created date field

--- components/blog/HeroArticles.vue
+++ components/blog/HeroArticles.vue
@@ -17,18 +17,18 @@
   <div class="d-flex flex-column flex-md-row mt-10 ga-5" v-else>
     <div class="flex-grow-1">
       <ArticleCard :href="getHref(blogEntry[0].slug)" :imgSrc="getImgSrc(blogEntry[0].imageUrl)"
-        :date="blogEntry[0].created_at_formated" :title="blogEntry[0].title" :description="blogEntry[0].summary"
+        :date="blogEntry[0].created_date" :title="blogEntry[0].title" :description="blogEntry[0].summary"
         :categories="blogEntry[0].categories" />
     </div>
 
     <div class="flex-grow-1">
 
       <ArticleCardSecondary :href="getHref(blogEntry[1].slug)" :imgSrc="getImgSrc(blogEntry[1].imageUrl)"
-        :date="blogEntry[1].created_at_formated" :title="blogEntry[1].title" :description="blogEntry[1].summary"
+        :date="blogEntry[1].created_date" :title="blogEntry[1].title" :description="blogEntry[1].summary"
         :categories="blogEntry[1].categories" class="mb-5" />
 
       <ArticleCardSecondary :href="getHref(blogEntry[2].slug)" :imgSrc="getImgSrc(blogEntry[2].imageUrl)"
-        :date="blogEntry[2].created_at_formated" :title="blogEntry[2].title" :description="blogEntry[2].summary"
+        :date="blogEntry[2].created_date" :title="blogEntry[2].title" :description="blogEntry[2].summary"
         :categories="blogEntry[2].categories" />
 
     </div>
